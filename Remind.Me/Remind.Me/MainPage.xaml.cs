531e6bf748f6e52436936ea822c5d4ed3811351e
Fábio Mourthé <fabio.clemente@vtex.com.br>
2014-05-28 23:14:51 -0300

    Create main page with pivots

--- Remind.Me/Remind.Me/MainPage.xaml.cs
+++ Remind.Me/Remind.Me/MainPage.xaml.cs
@@ -44,5 +44,10 @@ namespace Remind.Me
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Bring create page
+        }
     }
 }
