a6c21df1140af914e9f6358c655fe8abc2a5cff4
Fábio Mourthé <fabio.clemente@vtex.com.br>
2014-05-30 19:12:01 -0300

    Create Todo class

---
+++ Remind.Me/Remind.Me/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remind.Me
+{
+    public class Todo
+    {
+        public string title { get; private set; }
+        public string details { get; private set; }
+
+        public Todo(string title, string details)
+        {
+            this.title = title;
+            this.details = details;
+        }
+    }
+}
