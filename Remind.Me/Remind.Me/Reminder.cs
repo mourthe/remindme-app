0cc35e9660f667fa35811e4fae42223ecf9a7ce7
Fábio Mourthé <fabio.clemente@vtex.com.br>
2014-05-30 17:41:01 -0300

    Create Reminder Class

---
+++ Remind.Me/Remind.Me/Reminder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remind.Me
+{
+    public class Reminder
+    {
+        public string Title { get; private set; }
+        public string Details { get; private set; }
+        public string Local { get; private set; }
+        public bool Inactive { get; set; }
+
+        public Reminder(string title, string details, string local)
+        {
+            this.Title = title;
+            this.Details = details;
+            this.Local = local;
+            this.Inactive = false;
+        }
+    }
+}
