0312b6f582bac91b9896e4241164f98b8f4be14b
Fabio Mourthé <mourthe@live.com>
2015-01-11 17:49:23 -0200

    Add JSON.net nuget

---
+++ Remind.Me/Remind.Me/SettingsFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remind.Me
+{
+    public class SettingsFile
+    {
+        public double Distance { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public SettingsFile(double distance, double lat, double lon)
+        {
+            this.Distance = distance;
+            this.Longitude = lon;
+            this.Latitude = lat;
+        }
+    }
+}
