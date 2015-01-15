7cb3b6c5b293887f980dd8b42a53116d53e4295b
Fabio Mourthé <mourthe@live.com>
2015-01-15 00:02:35 -0200

    Create the DataSources for the tables

---
+++ Remind.Me/Remind.Me.Main/ReminderDataSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remind.Me.Database
+{
+    public class ReminderDataSource
+    {
+        private Helper _db;
+
+        public ReminderDataSource()
+        {
+            this._db = new Helper();
+        }
+
+        public async void AddReminder(Reminder reminder)
+        {
+            await _db.Conn.InsertAsync(reminder);
+        }
+
+        public async void RemoveReminder(string title)
+        {
+            var reminder = await _db.Conn.Table<Reminder>().Where(r => r.Title == title).FirstOrDefaultAsync();
+            if (reminder != null)
+            {
+                await _db.Conn.DeleteAsync(reminder);
+            }
+        }
+
+        public async void UpdateReminder(Reminder newReminder)
+        {
+            var Reminder = await _db.Conn.Table<Reminder>().Where(t => t.Title == newReminder.Title).FirstOrDefaultAsync();
+            if (Reminder != null)
+            {
+                _db.Conn.UpdateAsync(newReminder);
+            }
+        }
+
+        public async Task<List<Reminder>> FetchAllReminders()
+        {
+            return await _db.Conn.Table<Reminder>().ToListAsync();
+        }
+    }
+}
---
+++ Remind.Me/Remind.Me.Main/TodoDataSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remind.Me.Database
+{
+    public class TodoDataSource
+    {   
+        private Helper _db;
+
+        public TodoDataSource(Helper databaseHelper)
+        {
+            this._db = databaseHelper;
+        }
+
+        public async void AddTodo(Todo todo)
+        {
+            await _db.Conn.InsertAsync(todo);
+        }
+
+        public async void RemoveTodo(string title)
+        {
+            var todo = await _db.Conn.Table<Todo>().Where(t => t.Title == title).FirstOrDefaultAsync();
+            if (todo != null)
+            {
+                await _db.Conn.DeleteAsync(todo);
+            }
+        }
+
+        public async void UpdateTodo(Todo newTodo)
+        {
+            var todo = await _db.Conn.Table<Todo>().Where(t => t.Title == newTodo.Title).FirstOrDefaultAsync();
+            if (todo != null)
+            {
+                _db.Conn.UpdateAsync(newTodo);
+            }
+        }
+
+        public async Task<List<Todo>> FetchAllTodos()
+        {
+            return await _db.Conn.Table<Todo>().ToListAsync();
+        }
+    }
+}
