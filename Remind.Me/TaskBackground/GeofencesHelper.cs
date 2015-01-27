23f766067f655f7395817a394034ad9b5406a6da
Fabio Mourthé <mourthe@live.com>
2015-01-27 00:40:59 -0200

    Create the GeofenceTask project

---
+++ Remind.Me/TaskBackground/GeofencesHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
+
+namespace TaskBackground
+{
+    public static class GeofencesHelper
+    {
+        public static IList<Geofence> GetGeofences()
+        {
+            return GeofenceMonitor.Current.Geofences;
+        }
+
+        public static void CreateGeofence(string id, double lat, double lon, double radius)
+        {
+            if (GeofenceMonitor.Current.Geofences.SingleOrDefault(g => g.Id == id) != null) return;
+
+            var position = new BasicGeoposition();
+            position.Latitude = lat;
+            position.Longitude = lon;
+
+            var geocircle = new Geocircle(position, radius);
+
+            MonitoredGeofenceStates mask = 0;
+            mask |= MonitoredGeofenceStates.Entered;
+
+            // Create Geofence with the supplied id, geocircle and mask, not for single use
+            // and with a dwell time of 5 seconds
+            var geofence = new Geofence(id, geocircle, mask, false, new TimeSpan(0, 0, 5));
+            GeofenceMonitor.Current.Geofences.Add(geofence);
+        }
+
+        public static void RemoveGeofence(string id)
+        {
+            var geofence = GeofenceMonitor.Current.Geofences.SingleOrDefault(g => g.Id == id);
+
+            if (geofence != null)
+                GeofenceMonitor.Current.Geofences.Remove(geofence);
+        }
+    }
+}
---
+++ Remind.Me/TaskBackground/LocationTask.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Geolocation.Geofencing;
+using Windows.UI.Notifications;
+
+namespace TaskBackground
+{
+    public sealed class LocationTask : IBackgroundTask
+    {
+        static string TaskName = "MyLocationTask";
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Get the information of the geofence(s) that have been hit
+            var reports = GeofenceMonitor.Current.ReadReports();
+
+            GeofenceStateChangeReport report = null; 
+            foreach (var r in reports)
+                if (r.Geofence.Id == "MyGeofenceId" && r.NewState == GeofenceState.Entered)
+                    report = r;
+            
+            if (report == null) return;
+
+            // Create a toast notification to show a geofence has been hit
+            var toastXmlContent = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+            var txtNodes = toastXmlContent.GetElementsByTagName("text");
+            // modificar para pegar o texto
+            txtNodes[0].AppendChild(toastXmlContent.CreateTextNode("Geofence triggered toast!"));
+            txtNodes[1].AppendChild(toastXmlContent.CreateTextNode(report.Geofence.Id));
+
+            var toast = new ToastNotification(toastXmlContent);
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            toastNotifier.Show(toast);
+        }
+
+        public async static void Register()
+        {
+            if (!IsTaskRegistered())
+            {
+                var result = await BackgroundExecutionManager.RequestAccessAsync();
+                var builder = new BackgroundTaskBuilder();
+
+                builder.Name = TaskName;
+                builder.TaskEntryPoint = typeof(LocationTask).FullName;
+                builder.SetTrigger(new LocationTrigger(LocationTriggerType.Geofence));
+
+                builder.Register();
+            }
+        }
+
+
+        public static void Unregister()
+        {
+            var entry = BackgroundTaskRegistration.AllTasks.FirstOrDefault(kvp => kvp.Value.Name == TaskName);
+
+            if (entry.Value != null)
+                entry.Value.Unregister(true);
+        }
+
+        public static bool IsTaskRegistered()
+        {
+            var taskRegistered = false;
+            var entry = BackgroundTaskRegistration.AllTasks.FirstOrDefault(kvp => kvp.Value.Name == TaskName);
+
+            if (entry.Value != null)
+                taskRegistered = true;
+
+            return taskRegistered;
+        }
+    }
+}
